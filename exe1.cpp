49b8f06be8bd239df1e611829cc5d696848ae610
mkg-dev <maykegustavo@hotmail.com>
2020-12-28 20:25:19 -0300

    exemplo linguagem c

---
+++ exe1.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+
+
+
+
+main(){
+    
+  
+
+
+
+  int num = rand(60);
+    
+    printf("numero: ", %d);
+    
+  
+    
+    
+    
+    
+    
+    
+    
+    getch();
+}
